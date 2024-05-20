b044a9a83c10b8e673f9f03baddc2428ddd6ac10
cristopher estevez <cde1290@gmail.com>
2024-05-20 19:32:41 -0300

    agregue while

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-let monto=parseFloat(prompt("Ingrese monto a invertir: "));
-let tiempo=parseFloat(prompt("Ingrese los meses que invertira su dinero(teniendo en cuenta que hasta 6 meses el interes mensual es del 3%, luego aumenta al 5%):"));
-function inveriones(){
-if(tiempo<6){
-    let interesTotal=monto*tiempo*0.03;
-    let gananciaTotal=monto+interesTotal;
-    alert("Usted obtendrá al fianlizar el perdiodo un total de: $"+ interesTotal + " en intereses, obetiendo un total de: $ "+ gananciaTotal );
-    console.log("Intereses : "+interesTotal);
-    console.log("Ganancia total : "+gananciaTotal);
-}else if(tiempo>=6){
-    let interesTotal=monto*tiempo*0.05;
-    let gananciaTotal=monto+interesTotal;
-    alert("Usted obtendrá al fianlizar el perdiodo un total de: $"+ interesTotal + " en intereses, obetiendo un total de: $ "+ gananciaTotal );
-    console.log("Intereses : "+interesTotal);
-    console.log("Ganancia total : "+gananciaTotal);
+let continuar=true;
+while (continuar) {
+    let monto = parseFloat(prompt("Ingrese monto a invertir: "));
+    let tiempo = parseFloat(prompt("Ingrese los meses que invertirá su dinero (teniendo en cuenta que hasta 6 meses el interés mensual es del 3%, luego aumenta al 5%):"));
+    inversiones(monto, tiempo);
+    continuar = confirm("Presiona Aceptar para calcular su proxima inversion");
 }
-}
-inveriones();
+    function inversiones(monto,tiempo){
+    if(tiempo<6){
+        let interesTotal=monto*tiempo*0.03;
+        let gananciaTotal=monto+interesTotal;
+        alert("Usted obtendrá al fianlizar el perdiodo un total de: $"+ interesTotal + " en intereses y un total acumulado de: $ "+ gananciaTotal );
+        console.log("Intereses : "+interesTotal);
+        console.log("Ganancia total : "+gananciaTotal);
+    }else if(tiempo>=6){
+        let interesTotal=monto*tiempo*0.05;
+        let gananciaTotal=monto+interesTotal;
+        alert("Usted obtendrá al fianlizar el perdiodo un total de: $"+ interesTotal + " en intereses y un total acumulado de: $ "+ gananciaTotal );
+        console.log("Intereses : "+interesTotal);
+        console.log("Ganancia total : "+gananciaTotal);
+        
+    }
+    }
